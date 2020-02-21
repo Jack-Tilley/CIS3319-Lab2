d0822a18849052df4d42c2b108ebeea38844176e
Jack-Tilley <tilley.e.jack@gmail.com>
2020-02-20 21:16:40 -0500

    everything done besides receiver returning msg to sender

--- desencryptedchat/listenerThread.java
+++ desencryptedchat/listenerThread.java
@@ -21,6 +21,9 @@ public class listenerThread extends Thread{
     private volatile boolean endFlag;
     private String privateKey = "";
     private String hmacKey = "";
+    private static String ptReceived = "";
+    private static String ctReceived = "";
+    private static String hmacReceived = "";
     
     public listenerThread(Socket inSock){
         sock = inSock;
@@ -31,13 +34,19 @@ public class listenerThread extends Thread{
     public void run() {
         try{
             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+//            PrintWriter sendBack = new PrintWriter(sock.getOutputStream(), true);
             
             System.out.println("\nListener Thread Ready.");
             
             while(!endFlag){
                 String received = in.readLine(); //incoming ciphertext
                 String receivedHMAC = in.readLine();
+                String partnerPlainTextSent = in.readLine();
+                String partnerCypherTextSent = in.readLine();
+                
+                
 //                System.out.println("received cyphertext: " + received);
+                
                 
                 // DECRYPTION GOES HERE, PRINT OUT RESULTING PLAINTEXT INSTEAD OF received
                 
@@ -67,7 +76,19 @@ public class listenerThread extends Thread{
                 String pt = EncryptDecrypt.Decrypt(received, ReversedRoundKeyArray);
                 String printOut = ChatHelper.binaryStringToText(pt);
                 
+//                sendBack.println(printOut);
+//                sendBack.println(ChatHelper.binaryStringToText(received));
+//                sendBack.println(receivedHMAC);
+                
+                ptReceived = printOut;
+                ctReceived = ChatHelper.binaryStringToText(received);
+                hmacReceived = receivedHMAC;
+                set_ptReceived(ptReceived);
+                set_ctReceived(ctReceived);
+                set_hmacReceived(hmacReceived);
+                
                 String generatedHmac = "";
+                
                 try {
                     generatedHmac = EncryptDecrypt.generateHmac(printOut, hmacKey);
                 } catch (NoSuchAlgorithmException | InvalidKeyException ex) {
@@ -76,17 +97,20 @@ public class listenerThread extends Thread{
                 
                 System.out.println("---------------------------------------------------------------------------------------");
                 System.out.println(sock.getInetAddress().toString() + ": has sent: " + printOut);
-                System.out.println("\tPartner plain text sent: " + "ENTER HERE");
+                System.out.println("\tPartner plain text sent: " + partnerPlainTextSent);
+//                ptReceived = printOut;
                 
-                System.out.println("\tKey: " + privateKey);
-                System.out.println("\tHMAC Key: " + ChatHelper.textToBinaryString(hmacKey));
+                System.out.println("\tKey: " + ChatHelper.binaryStringToText(privateKey));
+                System.out.println("\tHMAC Key: " + hmacKey);
                 
 //                System.out.println("\tCyphertext received: " + received);
-                System.out.println("\tActual Cyphertext trasnlated from binary: " + ChatHelper.binaryStringToText(received));
-                System.out.println("\tPartner cypher text sent: " + "ENTER HERE");
+                System.out.println("\tActual Cyphertext sent: " + ChatHelper.binaryStringToText(received));
+                System.out.println("\tPartner cypher text sent: " + partnerCypherTextSent);
+//                ctReceived = ChatHelper.binaryStringToText(received);
                 
                 System.out.println("\tHMAC received: " + receivedHMAC);
                 System.out.println("\tHMAC generated: " + generatedHmac);
+//                hmacReceived = receivedHMAC;
                 
                 if (generatedHmac.equals(receivedHMAC)){
                     System.out.println("\tBoth HMACs match. This message is authentic.");      
@@ -117,6 +141,17 @@ public class listenerThread extends Thread{
     public void end(){
         endFlag = true;
     }
+    
+    public static void set_ptReceived(String pt){
+        DESencryptedChat.partnerPTReceived = pt;
+    }
+    public static void set_ctReceived(String ct){
+        DESencryptedChat.partnerCTReceived = ct;
+    }
+    public static void set_hmacReceived(String hmac){
+        DESencryptedChat.partnerHMACReceived = hmac;
+    }
 }
+
     
 
